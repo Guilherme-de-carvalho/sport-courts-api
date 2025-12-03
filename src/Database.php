106586d83f8c1e11dd90d39c4a84fdf09f1302d0
Guilherme-de-carvalho <gcgameplay40028922@gmail.com>
2025-12-03 14:30:39 -0300

    feat: remove cleanup_reservations script and add empty AuthMiddleware file

--- src/Database.php
+++ src/Database.php
@@ -9,7 +9,7 @@ class Database
     {
         $host = getenv('DB_HOST') ?: '127.0.0.1';
         $port = getenv('DB_PORT') ?: '3306';
-        $db   = getenv('DB_NAME') ?: 'sport_courts';
+        $db   = getenv('DB_NAME') ?: 'Local instance MySQL80';
         $user = getenv('DB_USER') ?: 'root';
         $pass = getenv('DB_PASS') ?: '';
 
